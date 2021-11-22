8481e74deb55641c04c1966af5642b71ab103ba2
DDamyan <ddminedd@gmail.com>
2021-11-22 16:35:25 +0100

    express initial
    -- added nodemon / express / ytdl-core / @types
    -- Express hello world
    -- npm script added "start-dev"

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,10 @@
-var lol;
-async function test() {
-    return lol;
-}
+const express = require('express');
+const app = express();
+const PORT = 3000;
+app.get('/', (req, res) => {
+    console.log('-Connected');
+    res.send('Hello World!');
+});
+app.listen(PORT, () => {
+    console.log(`=> Server listening at http://localhost:${PORT}`);
+});
--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,12 @@
-var lol: number;
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
-async function test() {
-  return lol;
-}
+app.get('/', (req, res) => {
+  console.log('-Connected');
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`=> Server listening at http://localhost:${PORT}`);
+});

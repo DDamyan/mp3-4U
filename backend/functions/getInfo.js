3d461dc8374dfe5c40cd50d294aa6d8c255f3621
DDamyan <ddminedd@gmail.com>
2021-11-23 13:38:20 +0100

    quick fix
    -- typescript compiles to newer version
    -- fixed typescript errors

--- backend/functions/getInfo.js
+++ backend/functions/getInfo.js
@@ -1,22 +1,11 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import ytdl from 'ytdl-core';
-export function getInfo(url) {
-    return __awaiter(this, void 0, void 0, function* () {
-        var info = yield ytdl.getInfo(url);
-        var { videoDetails } = info;
-        return {
-            title: videoDetails.title,
-            id: videoDetails.videoId,
-            url: videoDetails.video_url,
-            thumbnail: videoDetails.thumbnails[2],
-        };
-    });
+export async function getInfo(url) {
+    var info = await ytdl.getInfo(url);
+    var { videoDetails } = info;
+    return {
+        title: videoDetails.title,
+        id: videoDetails.videoId,
+        url: videoDetails.video_url,
+        thumbnail: videoDetails.thumbnails[2],
+    };
 }
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,3 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import express from 'express';
 const app = express();
 import { getInfo } from './functions/getInfo.js';
@@ -15,16 +6,16 @@ app.get('/', (req, res) => {
     console.log(' GET: /');
     res.send('Hello from server!');
 });
-app.get('/info', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get('/info', async (req, res) => {
     console.log(' GET: /info');
     let url = req.header('info');
     if (typeof url !== 'undefined') {
-        res.send(yield getInfo(url));
+        res.send(await getInfo(url));
     }
     else {
         res.end();
     }
-}));
+});
 app.listen(PORT, () => {
     console.log(`=> Server listening at http://localhost:${PORT}`);
 });
